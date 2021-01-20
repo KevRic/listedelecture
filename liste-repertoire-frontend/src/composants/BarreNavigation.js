6ca48b3ae666558917d8ce2ac94467412920f7f4
Sidi Jdide <jdide.sidi@gmail.com>
2021-01-20 13:47:07 -0500

    Modifier BareNavigation ( ajout le route de la page espace client

--- liste-repertoire-frontend/src/composants/BarreNavigation.js
+++ liste-repertoire-frontend/src/composants/BarreNavigation.js
@@ -5,7 +5,7 @@ import Nav from 'react-bootstrap/Nav';
 
 function BarreNavigation() {
     return (
-        <Navbar bg="light" expand="sm">
+        <Navbar bg="light" expand="sm" className="mb-4">
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="mr-auto">
@@ -15,17 +15,24 @@ function BarreNavigation() {
                     <LinkContainer to="/repertoire">
                         <Nav.Link>Repertoire</Nav.Link>
                     </LinkContainer>
+                   
+                    
+                    <LinkContainer to="/espaceClient">
+                        <Nav.Link variant={"info"}>Espace Client</Nav.Link>
+                    </LinkContainer>
                     <LinkContainer to="/admin">
                         <Nav.Link>Admin</Nav.Link>
                     </LinkContainer>
-                    <LinkContainer to="/demandeSpeciales">
-                        <Nav.Link>AjoutDemandeSpeciale</Nav.Link>
-                    </LinkContainer>  
-                    <LinkContainer to="/admin/listerDemandesSpeciales">
-                        <Nav.Link>ListerDemandesSpeciales</Nav.Link>
-                    </LinkContainer>
+
                 </Nav>
+                <LinkContainer to="/pageConnection">
+                       <Nav.Link className="mr-0">Connection</Nav.Link>
+                </LinkContainer>
+                <LinkContainer to="/pageRegistre">
+                       <Nav.Link className="mr-0">Registre</Nav.Link>
+                </LinkContainer>
             </Navbar.Collapse>
+            
         </Navbar>
     );
 }
