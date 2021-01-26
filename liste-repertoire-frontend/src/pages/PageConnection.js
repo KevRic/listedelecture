cf34295481a265d33b4a4437d1a5a82c3745819b
Sidi Jdide <jdide.sidi@gmail.com>
2021-01-26 08:11:14 -0500

    Modifie pageConnection ( enlever listeUtilisateurs et listeAdmis mettre client avant admin)

--- liste-repertoire-frontend/src/pages/PageConnection.js
+++ liste-repertoire-frontend/src/pages/PageConnection.js
@@ -2,46 +2,22 @@
 import FormulaireConnectionAdmin from '../composants/FormulaireConnectionAdmin'
 import FormulaireConnectionUtilisateur from '../composants/FormulaireConnectionUtilisateur'
 import Alert from 'react-bootstrap/Alert'
-import { React,useState,useEffect} from 'react'
+import { React} from 'react'
 import Tabs from 'react-bootstrap/Tabs';
-//const { Formik } = formik;
 import Tab from 'react-bootstrap/Tabs';
-import Sonnet from 'react-bootstrap/Tabs';
-
 function PageConnection(){
-
-  const [listeUtilisateurs, setListeUtilisateurs] = useState([]);
-  const [listeAdmins, setListeAdmins] = useState([]);
-  useEffect(() => {
-      const chercherDonnees = async () => {
-          const resultat = await fetch(`/api/utilisateurs`);
-          const body = await resultat.json().catch((error) => {console.log(error)});
-          setListeUtilisateurs(body);
-      };
-      chercherDonnees();
-  }, []);
-
-  useEffect(() => {
-    const chercherDonnees = async () => {
-        const resultat = await fetch(`/api/admins`);
-        const body = await resultat.json().catch((error) => {console.log(error)});
-        setListeAdmins(body);
-    };
-    chercherDonnees();
-}, []);
-
 
     return(
 
         <>
           <Tabs defaultActiveKey="admin" transition={false} id="choixFormulaire">
-                <Tab eventKey="admin" title="Client">
-                <Alert variant={"info mt-4 mb-4"}> Page connection admin  </Alert>
-                   <FormulaireConnectionAdmin admins={listeAdmins}/>
-                </Tab>
                 <Tab eventKey="utilisateur" title="Client">
                 <Alert variant={"info mt-4 mb-4"}> Page de connection client </Alert>
-                    <FormulaireConnectionUtilisateur utilisateurs={listeUtilisateurs}/>
+                    <FormulaireConnectionUtilisateur />
+                </Tab>
+                <Tab eventKey="admin" title="Admin">
+                <Alert variant={"info mt-4 mb-4"}> Page connection admin  </Alert>
+                   <FormulaireConnectionAdmin/>
                 </Tab>
         </Tabs>
         </>
