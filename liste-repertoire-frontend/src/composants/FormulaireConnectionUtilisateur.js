5222f15c00bf8527b8cbdc8f4ad56da148ef6c70
Sidi Jdide <jdide.sidi@gmail.com>
2021-01-26 08:26:50 -0500

    modifie formulaireConnectionUtilisateur ( ajout des préconditions , utilisation de méthode  "post" pour envoyer les informations de l'utilisateur au serveur )

--- liste-repertoire-frontend/src/composants/FormulaireConnectionUtilisateur.js
+++ liste-repertoire-frontend/src/composants/FormulaireConnectionUtilisateur.js
@@ -1,86 +1,77 @@
 
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
-import { React,useState} from 'react'
+import { React,useState,useEffect} from 'react'
 import Alert from 'react-bootstrap/Alert'
 import Table from 'react-bootstrap/Table'
 import {UtiliseAUTH} from '../Context/Auth'
 import { Redirect } from 'react-router-dom';
 import { Link } from 'react-router-dom';
-function FormulaireConnectionUtilisateur({utilisateurs}){
-    const {setAuthentification}=UtiliseAUTH();
 
-    const [nom, setNom] = useState('');
-    const [prenom, setPrenom] = useState('');
+
+function FormulaireConnectionUtilisateur(){
+
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [alertConnection, setAlertConnection] = useState('');
     const [alertColorReponse, setAlertColorReponse] = useState('');
     const [rediriger, setRediriger] = useState(false);
-    
-    function  AfficherUtilisateurs() {
-        return( 
-           <>
-           <h4>Utilisateurs disponible : </h4>
-            <Table striped bordered hover >
-            <thead>
-                <tr>
-                <th>Nom</th>
-                <th>Prenom</th>
-                <th>Email</th>
-                <th>Password</th>
-                
-                </tr>
-            </thead>
-            {
-                utilisateurs.map((utilisateur)=>
-                
-                    <tbody >
-                    <tr>
-                    <td>{utilisateur.Nom}</td>
-                    <td>{utilisateur.Prenom}</td>
-                    <td>{utilisateur.Email}</td>
-                    <td>{utilisateur.Password}</td>
-                    </tr>
-                    
-                </tbody>
+    const [utilisateurActuelle,setUtilisateurActuel]=useState({})
+    const {setAuthentification}=UtiliseAUTH();
 
-                )
+       const  SeConnecter = async () => {
+            if(email.length===0|| password.length===0){
+
+                setAlertConnection("Attention tous les champs sont obligatoires ");
+                setAlertColorReponse("danger");
             }
-           
-         </Table>
-            </>
-        )
-        
-    }
+            else if(password.length<6)
+            {
+                setAlertConnection("Mot de passe doit être 6 caractéres ou plus ");
+                setAlertColorReponse("danger");
+            }
+            else
+            {
 
+                await fetch(`/api/utilisateurs/authentifier`,
+                {
+                    method: 'post',
+                    body: JSON.stringify({Email:email,Password:password}),
+                    headers: {
+                        'Content-Type': 'application/json'
+                    }
+                    
+                }
+                ).then( (response) => {response.json().then(
+                    
+                    data=> {
+                        if(response.status===200){
+                            setAuthentification(true);
+                            setUtilisateurActuel({_d:data._id,Nom:data.Nom,Prenom:data.Prenom,Email:data.Email,Password:data.Password});
+                            console.log(utilisateurActuelle);
+                            setAlertConnection("Vous êtes identifiés !!!");
+                            setAlertColorReponse("success");
+                        }
+                        else 
+                        {
+                            setAuthentification(false);
+                            setAlertConnection("l'utilisateur n'existe pas ");
+                            setAlertColorReponse("danger");
+                        }
+                        
+                        console.log(response.status)
+                } )})
+            };
+            }
+        
+        
+    
     function AfficherRedirection() {
         if (rediriger === true) {
             return <Redirect to="/espaceClient" />
         }
     }
-    function SeConnecter(){
-        
-
-        utilisateurs.forEach(utilisateur => {
-
-          if (email.length===0 && password.length===0){
-                setAlertConnection("Attention tous les champs sont obligatoire");
-                setAlertColorReponse("danger");
-              }
-          else  if(email===utilisateur.Email && password===utilisateur.Password)
-              {
-                setAlertConnection("Informations validés vous êtes identifiés");
-                setAlertColorReponse("success");
-                setAuthentification(true);
-                setRediriger(true);
-              }
-            else{
-                setAlertConnection("Nom utilisateur ou mot de passe incorrect");
-                setAlertColorReponse("danger");
-            }
-            })
-    }
+    
     return(
 
         <>
@@ -89,33 +80,24 @@ function FormulaireConnectionUtilisateur({utilisateurs}){
     <Form className="width">
             <Form.Group controlId="formBasicEmail">
                 <Form.Label>Nom utilisateur :  </Form.Label>
-                <Form.Control type="email" value={email} placeholder="Entrer nom utilisateur ou email" 
-                   onChange={(event) => setEmail(event.target.value)} />
-                <Form.Text className="text-muted">
-                
-                </Form.Text>
+                <Form.Control type="email" value={email} required
+                  
+                   onChange={(event) => setEmail(event.target.value)}  />
+                   <Form.Control.Feedback type="invalid">
+                     Saisie un email valid svp.
+                 </Form.Control.Feedback>
             </Form.Group>
 
             <Form.Group controlId="formBasicPassword">
                 <Form.Label>Mot de passe : </Form.Label>
-                <Form.Control type="password"  placeholder="Password" value={password}
+                <Form.Control type="password"  required value={password}
                  onChange={(event) => setPassword(event.target.value)}  />
             </Form.Group>
-            <Form.Group controlId="formBasicCheckbox">
-                <Form.Check type="checkbox" label="Check me out" />
-            </Form.Group>
-            <Button variant="primary" onClick={() =>SeConnecter()} >
-                Connecter
-            </Button>
-            <Link to="/PageRegistre">
-             <Button variant="info" className="ml-2" >
-                Registre
-             </Button>
-            </Link>
+            <Button variant="primary"  onClick={()=>SeConnecter()} > Connecter</Button>
+            <Link to="pageRegistre" className="btn btn-link pr-0">Registre?</Link>
             
  </Form>
-   <Alert variant={alertColorReponse} className="mt-4 mb-4">{alertConnection}</Alert>
-   <AfficherUtilisateurs/>
+   <Alert variant={alertColorReponse} className="mt-4 mb-4"> {alertConnection}</Alert>
         </>
     )
 }
