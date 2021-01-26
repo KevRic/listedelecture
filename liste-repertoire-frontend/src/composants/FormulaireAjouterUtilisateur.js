369c5d5aaee99132d4aaa6628b3ddd70c115643f
Sidi Jdide <jdide.sidi@gmail.com>
2021-01-26 08:17:50 -0500

    Modifier FormulaireAjouterUtilisateur ( ajout des préconditions , Vérification si l'email déja ajouter ou non )

--- liste-repertoire-frontend/src/composants/FormulaireAjouterUtilisateur.js
+++ liste-repertoire-frontend/src/composants/FormulaireAjouterUtilisateur.js
@@ -1,7 +1,3 @@
-import  Form  from'react-bootstrap/Form'
-import  Button from'react-bootstrap/Button'
-import InputGroup from 'react-bootstrap/InputGroup'
-import Col from 'react-bootstrap/Col'
 
 import {
   React,
@@ -9,107 +5,127 @@ import {
   useEffect
 } from 'react';
 
-import { Redirect } from 'react-router-dom';
+import { Form } from 'react-bootstrap';
+import Button from 'react-bootstrap/Button';
+import Col from 'react-bootstrap/Col'
+import Alert from 'react-bootstrap/Alert'
+import { Redirect,Link } from 'react-router-dom';
 
-
-function FormularireAjouterUtilisateur(){
-  const [nom, setNom] = useState('');
+function FormulaireAjouterUtilisateur() {
+  const [validated, setValidated] = useState(false);
   const [prenom, setPrenom] = useState('');
+  const [nom, setNom] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirm] = useState('');
   const [rediriger, setRediriger] = useState(false);
-  const [validated, setValidated] = useState(false);
+  const [alertRegistre, setAlertRegistre] = useState("");
+  const [alertColorRegistre, setAlertColorRegistre] = useState("light");
+  const [etatBouton, setEtatBouton] = useState(true);
 
   const AjouterUtilisateur = async () => {
+
     await fetch(`/api/utilisateurs/ajouter`, {
         method: 'post',
-        body: JSON.stringify({ Nom: nom, Prenom: prenom,Email :email, Password:password  }),
+        body: JSON.stringify({ Nom: nom, Prenom: prenom, Email: email,Password:password  }),
         headers: {
             'Content-Type': 'application/json'
         }
-    });
+    }).then((response)=>{
+      if(response.status===200){
+        setAlertRegistre("l'utilisateur est ajouté avec succes");
+        setAlertColorRegistre("success");
+      }else if(response.status===406){
+        setAlertRegistre("Le email existe deja veuillez choisir un nouveau email");
+        setAlertColorRegistre("warning");
+      }
+      else{
+        setAlertRegistre("L'utilisateur n'a pas été ajoutée");
+        setAlertColorRegistre("danger");
+      }
+    })
     setRediriger(true);
+    setEtatBouton(true);
 };
-function AfficherRedirection() {
-  if (rediriger === true) {
-      return <Redirect to="/" />
-  }
-}
-    const handleSubmit = (event) => {
-        const form = event.currentTarget;
-        if (form.checkValidity() === false) {
-          event.preventDefault();
-          event.stopPropagation();
+
+function VerifierDonnesEntresParUtilisateur(){
+      if (nom.length===0 || prenom.length===0 || email.length===0 || confirmPassword.length===0) {
+          setAlertRegistre("Tous les champs sont obligatoire");
+          setAlertColorRegistre("danger");
+          setEtatBouton(true);
+      }
+  else if (password!==confirmPassword){
+          setAlertRegistre("mots de passe différentes");
+          setAlertColorRegistre("danger");
+          setEtatBouton(true);
+      }
+  else if(password.length<6)
+       {
+              setAlertRegistre("Mot de passe doit être 6 caractéres ou plus ");
+              setAlertColorRegistre("danger");
         }
+  else{
+      setAlertRegistre("Information validés");
+      setAlertColorRegistre("success");
+      setEtatBouton(false);
+        
+      }
+      
+  }
+ 
     
-        setValidated(true);
-      };
-    
-    return(
+  const handleSubmit = (event) => {
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
 
-        <>
+    setValidated(true);
+  };
 
-        <Form noValidate validated={validated} onSubmit={handleSubmit}>
+  return (
+      <>
+    <Form noValidate validated={validated} onSubmit={handleSubmit}>
       <Form.Row>
         <Form.Group as={Col} md="4" controlId="validationCustom01">
           <Form.Label>Prénom</Form.Label>
-          <Form.Control
-            required
-            type="text" value={prenom} onChange={(event) => setPrenom(event.target.value)}
-            placeholder="Prénom"
-            defaultValue="Mark"
-          />
-          <Form.Control.Feedback>Correct!</Form.Control.Feedback>
+          <Form.Control required type="text" value={prenom} onChange={(event) => setPrenom(event.target.value)} />
+          <Form.Control.Feedback>c'est ok </Form.Control.Feedback>
         </Form.Group>
         <Form.Group as={Col} md="4" controlId="validationCustom02">
           <Form.Label>Nom</Form.Label>
-          <Form.Control
-            required
-            type="text" value={nom} onChange={(event) => setNom(event.target.value)}
-            placeholder="Nom"
-            defaultValue="Otto"
-          />
-          <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
-        </Form.Group>
-        <Form.Group as={Col} md="4" controlId="validationCustomUsername">
-          
+          <Form.Control required type="text" value={nom} onChange={(event) => setNom(event.target.value)}/>
+          <Form.Control.Feedback>c'est ok </Form.Control.Feedback>
         </Form.Group>
       </Form.Row>
       <Form.Row>
         <Form.Group as={Col} md="6" controlId="validationCustom03">
           <Form.Label>Email</Form.Label>
-          <Form.Control type="Email" value={email} onChange={(event) => setEmail(event.target.value)} placeholder="email" required />
-          <Form.Control.Feedback type="invalid">
-            Entrer votre email svp.
-          </Form.Control.Feedback>
+          <Form.Control type="email"  required value={email} onChange={(event) => setEmail(event.target.value)} />
         </Form.Group>
         <Form.Group as={Col} md="3" controlId="validationCustom04">
           <Form.Label>Password</Form.Label>
-          <Form.Control type="Password" value={password}  onChange={(event) => setPassword(event.target.value)}placeholder="State" required />
-          <Form.Control.Feedback type="invalid">
-            Entrer mot de passe
+          <Form.Control type="password" required value={password} onChange={(event) => setPassword(event.target.value)}/>
+          <Form.Control.Feedback length="6" type="invalid">
           </Form.Control.Feedback>
         </Form.Group>
         <Form.Group as={Col} md="3" controlId="validationCustom05">
-          <Form.Label>Confirmer</Form.Label>
-          <Form.Control type="Password" placeholder="" required />
-          <Form.Control.Feedback type="invalid">
-            Mots de passe différent
-          </Form.Control.Feedback>
+          <Form.Label>Confirm pasword</Form.Label>
+          <Form.Control type="password"  required value={confirmPassword} onChange={(event) => setConfirm(event.target.value)} />
         </Form.Group>
       </Form.Row>
       <Form.Group>
-        <Form.Check
-          required
-          label="Confirmer"
-          feedback="Vous devrez confirmer avant d'envoyer."
-        />
       </Form.Group>
-      <Button onClick={AjouterUtilisateur}>Registrer</Button>
+      <Button  className="mr-4" onClick={()=>VerifierDonnesEntresParUtilisateur()}>Vérifier</Button>
+      <Button variant="primary" disabled={etatBouton} onClick={AjouterUtilisateur} >Enregistre</Button>
+       <Link to="pageConnection" className="btn btn-link pr-0">Connection?</Link>
+      
     </Form>
-        
-        </>
-    )
-};
+   
+    <Alert className="mt-4" variant={alertColorRegistre}>{alertRegistre}</Alert>
+    </>
+  );
+}
 
-export default FormularireAjouterUtilisateur;
+export default FormulaireAjouterUtilisateur;
